4ac4259d3bcde3dae3a60c372ce29549f7187bc7
Tejas M R <matblockgreninja@gmail.com>
2021-02-23 14:28:57 +0530

    Initial Commit

---
+++ CustomLinearProgressView/CustomLinearProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLinearProgressViewApp.swift
+//  CustomLinearProgressView
+//
+//  Created by tezz on 23/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomLinearProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
